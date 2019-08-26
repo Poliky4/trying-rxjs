37841c701213db077769901e80f043ad44618ccc
Poliky4 <pontusmolin@me.com>
2019-08-26 11:22:21 +0200

    separate static and dynamic styles

--- src/render.js
+++ src/render.js
@@ -7,64 +7,86 @@ const GameDiv = styled.div`
   height: 100%;
 `;
 
-const Entity = styled.div(({ x, y, width, height }) => {
-  
-  return `
-    position: absolute;
-    left: ${x}%;
-    top: ${y}%;
-    width: ${width}vw;
-    height: ${height}vh;
-    background-color: yellow;
-  `;
-});
-
-const Ground = styled(Entity)`
-    background-color: darkgreen;
+const Entity = styled.div`
+  position: absolute;
+  width: ${({width}) => width}vmin;
+  height: ${({height}) => height}vmin;
 `;
 
-const Player = styled.div(({ x, y, z, width, height }) => {
-  
-  const calculatedZ = z * 5;
-
-  return `
-    position: absolute;
-    left: ${x}%;
-    top: ${y}%;
-    border-radius: 50%;
-    width: ${width}vmin;
-    height: ${height}vmin;
-    background-color: darkred;
-    transform: translate(-50%, ${50 - calculatedZ}%);
-    transition-duration: 100ms;
-    transition-timing-function: linear;
-    transition-property: left, top transform;
-
-    ::after {
-      content: "";
-      display: block;
-      position: absolute;
-      top: -20px;
-      left: 20px;
-      width: ${width}vmin;
-      height: ${height}vmin;
-      background-color: rgba(0, 0, 0, 0.2);
-    }
-  `;
+const StyledPlayer = styled(Entity)`
+  border-radius: 50%;
+  background-color: darkred;
+  transition-duration: 250ms;
+  transition-timing-function: linear;
+  transition-property: left, top, transform;
+`;
+const StyledShadow = styled(StyledPlayer)`
+  background-color: rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 40px rgba(0, 0, 0, 0.2);
+`;
+const Shadow = (({style, z, ...props}) => {
+  const zz = (z/1000);
+  return (
+    <StyledShadow
+      style={{
+        ...style,
+        transform: `
+          translate(-60%, 90%)
+	  scale(${0.4 + zz})
+        `,
+      }}
+      {...props}
+    />
+  );
+});
+const Player = (player => {
+  const { x, y, z, ...props } = player;
+  const zx = z * 0.5;
+  const zy = z * 3;
+  return (
+    <>
+      <Shadow
+        style={{
+          left: `${x}%`,
+          top: `${y}%`,
+        }}
+        {...player}
+      />
+      <StyledPlayer 
+        style={{
+          left: `${x}%`,
+          top: `${y}%`,
+          transform: `translate(${-50 + zx}%, ${50 - zy}%)`,
+        }}
+        {...player}
+      />
+    </>
+  );
 });
 
 const p = num => num.toFixed(0);
+const StyledInfo = styled.div`
+  display: inline-block;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: darkorange;
+`;
+const Info = ({ x, y, z, vx, vy, vz }) => {
+  return (
+    <StyledInfo>
+      <p>{`pos: (${p(x)},${p(y)},${p(z)})`}</p>
+      <p>{`vel: (${p(vx)},${p(vy)},${p(vz)})`}</p>
+    </StyledInfo>
+  );
+};
 const Render = ({ data }) => {
-  const { player, ground, map } = data;
-  const { x, y, z, vx, vy, vz } = player;
+  const { player } = data;
   return (
     <GameDiv>
-      <Ground {...ground} />
       <Player {...player} />
-      <div>
-        <p>{`pos: (${p(x)},${p(y)},${p(z)})`}</p>
-        <p>{`vel: (${p(vx)},${p(vy)},${p(vz)})`}</p>
-      </div>
+      <Info {...player}  />
     </GameDiv>
   );
 }
